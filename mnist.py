0e8dd485a317b3f659233a14f11344e4ed5ec185
filippofilip95 <fpapranecx@gmail.com>
2018-05-04 19:58:10 +0200

    :metal: improved mnist accuracy

--- mnist.py
+++ mnist.py
@@ -8,6 +8,7 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras import utils
 from keras.datasets import mnist
+from keras.utils import plot_model
 
 def clear_console():
     clear = "\n" * 100
@@ -16,11 +17,32 @@ def clear_console():
 def show_image(image):
     image = np.array(image, dtype='float')
     pixels = image.reshape((28, 28))
+    fig = plt.figure()
+    plt.legend([None])
     plt.imshow(pixels, cmap='gray')
-    plt.show()
+    fig.savefig('./src/images/mnist_digit_to_predicate.png')
 
-def to_percentage(number):
-    return round(number,2)*100
+
+def show_learning_rate_graphs(history):
+    # summarize history for accuracy
+    fig = plt.figure()
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    fig.savefig('./src/images/mnist_acc_history.png')
+
+    # summarize history for loss
+    fig = plt.figure()
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    fig.savefig('./src/images/mnist_loss_history.png')
 
 def preprocess_data(train_images, test_images):
     # Reshape data to 1 color channel
@@ -43,23 +65,18 @@ def preprocess_labels(train_labels, test_labels):
 
     return train_labels, test_labels
 
-def main():
-    # download and load MNIST dataset
-    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-
-    # prepare data for neural network
-    train_images, test_images = preprocess_data(train_images, test_images)
-    train_labels, test_labels = preprocess_labels(train_labels, test_labels)
-
+def create_and_compile_model():
     # define Sequential model
     network_model = Sequential()
 
-    # add Convolution layers
-    network_model.add(Convolution2D(32, 3, strides=3, activation='relu', input_shape=(1,28,28),  padding="same"))
-    network_model.add(Convolution2D(32, 3, strides=3, activation='relu',  padding="same"))
+    # add Convolution layer
+    network_model.add(Convolution2D(30, (5,5), activation='relu', input_shape=(1,28,28),  padding="same"))
 
     # add Pooling to reduce parameters
     network_model.add(MaxPooling2D(pool_size=(2,2), padding="same"))
+
+    # add Convolution layer
+    network_model.add(Convolution2D(15, (3,3), activation='relu', padding="same"))
 
     # add Dropout to prevent overfitting
     network_model.add(Dropout(0.25))
@@ -69,27 +86,40 @@ def main():
 
     # add Dense layers => neuron to neuron
     network_model.add(Dense(128, activation='relu'))
-    network_model.add(Dropout(0.5))
     network_model.add(Dense(10, activation='softmax'))
 
     # compile our network model
     network_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # save model visualization to file
+    plot_model(network_model, to_file='./src/images/mnist_model.png', show_shapes=True)
 
-    print('\nTeaching our neural network started!\n')
+    return network_model
+
+
+def main():
+    # download and load MNIST dataset
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+    # prepare data for neural network
+    train_images, test_images = preprocess_data(train_images, test_images)
+    train_labels, test_labels = preprocess_labels(train_labels, test_labels)
+
+    network_model = create_and_compile_model()    
+
+    print('\nTeaching of neural network started!\n')
     starting_time = time.time()
 
     # train our network model
-    network_model.fit(train_images, train_labels, batch_size=32, verbose=1, epochs=10)
-    
+    history = network_model.fit(train_images, train_labels, batch_size=200, verbose=1, epochs=10, validation_data=(test_images,test_labels))
+    show_learning_rate_graphs(history)
+
     ending_time = time.time()
     teaching_time = round((ending_time - starting_time),2)
     print('\nTeaching of neural network took {} seconds.\n'.format(teaching_time))
 
     # evaluate test data on trained model
     score = network_model.evaluate(test_images, test_labels, verbose=0)
-    
-    success_percentage = to_percentage(score[1])
-    print('Neural network predicts with {}% success.\n'.format(success_percentage))
+    print('Neural network predicts with {}% success.\n'.format(score[1]*100))
 
     # make a prediction
     start_index = np.random.randint(0,100)
